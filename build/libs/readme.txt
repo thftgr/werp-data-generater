7a69c7c8e18a5fe5e39805e371fc90ea0933f2c4
thftgr <44352170+thftgr@users.noreply.github.com>
2020-09-05 16:22:46 +0900

    123

---
+++ build/libs/readme.txt
@@ -0,0 +1,6 @@
+연결실패시 파일에는 유지 
+java -jar "werp-1.0-SNAPSHOT.jar" 7e2757b7-badc-4303-8cd0-2eefa9d78e3b
+
+연결실패시 파일에서도 삭제
+java -jar "werp-1.0-SNAPSHOT.jar" 7e2757b7-badc-4303-8cd0-2eefa9d78e3b -d
+
--- src/main/java/com/thftgr/Main.java
+++ src/main/java/com/thftgr/Main.java
@@ -9,7 +9,7 @@ import okhttp3.*;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.InetAddress;
+import java.net.*;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -21,41 +21,71 @@ public class Main {
     static long startTime = System.currentTimeMillis();
 
     public static void main(String[] args) {
-        try {
-            proxyList = (JsonArray) JsonParser.parseReader(new Gson().newJsonReader(new FileReader("setting/proxy_list.json")));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        if (args[0].length() != 36) {
+            System.out.println("data length not match");
+            return;
         }
+
+
+            try {
+                proxyList = (JsonArray) JsonParser.parseReader(new Gson().newJsonReader(new FileReader("setting/proxy_list.json")));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
 
 
         int dataCount = 0;
         long timetmp = 0;
+        long delay = 0;
         while (true) {
+            delay = System.currentTimeMillis();
+            if (proxyList.size() == 0) {
+                System.out.println("proxy list end");
+                return;
+            }
             for (int i = 0; i < proxyList.size(); i++) {
+
+
                 timetmp = System.currentTimeMillis();
-                new Main().setProxy(proxyList.get(i).getAsString());
+                System.out.println("=====================================");
+
+                Proxy proxy = new Main().buildProxy(proxyList.get(i).getAsString());
+                System.out.println(proxy.toString());
+
 
                 try {
-                    Thread.sleep(5000);
-
-                    System.out.println("=====================================");
-                    System.out.println(System.getProperty("https.proxyHost"));
-                    if (new Main().generator("cf62e033-9e89-49c6-9830-5e61adc29ac5")) {
-//                    if (new Main().generator("7e2757b7-badc-4303-8cd0-2eefa9d78e3b")) {
+//                    if (new Main().generator("cf62e033-9e89-49c6-9830-5e61adc29ac5",proxy)) {
+//                    if (new Main().generator("7e2757b7-badc-4303-8cd0-2eefa9d78e3b", proxy)) {
+                    if (new Main().generator(args[0], proxy)) {
                         dataCount += 1;
                         System.out.println("generated data : " + dataCount + "GB");
                         System.out.println((System.currentTimeMillis() - timetmp) + "ms\n");
-                        System.out.println(((System.currentTimeMillis() - startTime) / dataCount) + "Ms/GB\n");
+                        System.out.println(((System.currentTimeMillis() - startTime) / dataCount / 1000) + "s/GB\n");
                     } else {
                         System.out.println("generate fail. | data : " + dataCount + "GB");
-
                     }
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
+                    System.out.println();
                     System.out.println("Exception generate fail. | data : " + dataCount + "GB");
+                    proxyList.remove(i); //램에서만 삭제
+                    if(args.length == 2){
+                        if (args[1].equals("-d")) {
+                            new Main().settingSave();
+                        }
+                    }
+
 
                 }
 
+            }
+            // 전체 실행 후 시간이 20초 이하면 남운시간만큼 대기
+            if (System.currentTimeMillis() - delay < 20000) {
+                try {
+                    Thread.sleep((System.currentTimeMillis() - delay));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -90,8 +120,12 @@ public class Main {
         return iostime.substring(0, iostime.length() - 1) + "+09:00";
     }
 
-    boolean generator(String referrer_id) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
+    boolean generator(String referrer_id, Proxy proxy) throws IOException {
+        OkHttpClient client = new OkHttpClient()
+                .newBuilder()
+                .proxy(proxy)
+                .build();
+
 
         String installId = new Main().rand(11, false);
 
@@ -124,24 +158,11 @@ public class Main {
         return !resp.get("referrer").isJsonNull();
     }
 
-    JsonArray updateProxyList(String url) {
-        JsonArray list = new JsonArray();
-        /*
-            call rest api
-            parse string data to json array
-                [
-                    {"ip":"String","port":"int"},
-                    {"ip":"String","port":"int"},
-                    {"ip":"String","port":"int"},
-                ]
-         */
-        return list;
-    }
 
-
-    void setProxy(String ip_port) {
-        System.setProperty("https.proxyHost", ip_port.substring(0,ip_port.indexOf(":")));
-        System.setProperty("https.proxyPort", ip_port.substring(ip_port.indexOf(":")+1));
+    Proxy buildProxy(String ip_port) {
+        String ip = ip_port.substring(0, ip_port.indexOf(":"));
+        int port = Integer.parseInt(ip_port.substring(ip_port.indexOf(":") + 1));
+        return new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
     }
 
     void settingSave() {
@@ -154,7 +175,6 @@ public class Main {
             e.printStackTrace();
         }
     }
-
 
 
 }
